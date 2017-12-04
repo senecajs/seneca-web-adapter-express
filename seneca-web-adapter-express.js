735d98718a0bfc437eb313b47fd811bee0c1cfc0
Tyler Waters <tswaters@users.noreply.github.com>
2017-12-03 19:52:55 -0800

    Adds support for middleware
    * Update travis to run on node@8
    * Bump lab@13 to fix WebAssembly leak
    * Bump seneca-web@latest for middleware
    * Adds support for middleware
    * Adding package-lock.json
    
    Fixes #13

--- seneca-web-adapter-express.js
+++ seneca-web-adapter-express.js
@@ -12,10 +12,23 @@ module.exports = function express (options, context, auth, routes, done) {
     return done(new Error('no context provided'))
   }
 
+  // middleware is an object with keys defining middleware
+  const middleware = options.middleware
+
   // Routes have methods as an array, we need
   // to loop once per route, per method to get
   // all of the routes correctly.
   _.each(routes, (route) => {
+    // pull out middleware from the route; map strings to options' middleware.
+    // if we don't get a function, blow up hard - this is a user-code problem.
+    const routeMiddleware = (route.middleware || []).map(_middleware => {
+      const ret = _.isString(_middleware) ? middleware[_middleware] : _middleware
+      if (!_.isFunction(ret)) {
+        throw new Error(`expected valid middleware, got ${_middleware}`)
+      }
+      return ret
+    })
+
     _.each(route.methods, (method) => {
       // lowercase the method so we can use it
       // ala server[method] aka server.get()
@@ -25,13 +38,13 @@ module.exports = function express (options, context, auth, routes, done) {
       // on if the route is for authorization, is
       // secure, or neither. Each is explained below.
       if (!route.auth && !route.secure) {
-        unsecuredRoute(seneca, options, context, method, route)
+        unsecuredRoute(seneca, options, context, method, routeMiddleware, route)
       }
       else if (route.secure) {
-        securedRoute(seneca, options, context, method, route)
+        securedRoute(seneca, options, context, method, routeMiddleware, route)
       }
       else if (route.auth) {
-        authRoute(seneca, options, context, method, route, auth)
+        authRoute(seneca, options, context, method, route, routeMiddleware, auth)
       }
     })
   })
@@ -86,36 +99,47 @@ function handleRoute (seneca, options, request, reply, route, next) {
 }
 
 // Unsecured routes just call the handler directly, no magic.
-function unsecuredRoute (seneca, options, context, method, route) {
-  context[method](route.path, (request, reply, next) => {
-    handleRoute(seneca, options, request, reply, route, next)
-  })
+function unsecuredRoute (seneca, options, context, method, middleware, route) {
+  const routeArgs = [route.path]
+    .concat(middleware)
+    .concat([(request, reply, next) => {
+      handleRoute(seneca, options, request, reply, route, next)
+    }])
+
+  context[method].apply(context, routeArgs)
 }
 
 // Auth routes call passport.authenticate and can and do redirect
 // depending on pass or failure. Generally this means you don't
 // need a seneca handler set as the redirects happend instead.
-function authRoute (seneca, options, context, method, route, auth) {
+function authRoute (seneca, options, context, method, route, middleware, auth) {
   const opts = {
     failureRedirect: route.auth.fail,
     successRedirect: route.auth.pass
   }
 
-  const strategy = auth.authenticate(route.auth.strategy, opts)
-  context[method](route.path, strategy, (request, reply, next) => {
-    handleRoute(seneca, options, request, reply, route, next)
-  })
+  const routeArgs = [route.path]
+    .concat([auth.authenticate(route.auth.strategy, opts)])
+    .concat(middleware)
+    .concat([(request, reply, next) => {
+      handleRoute(seneca, options, request, reply, route, next)
+    }])
+
+  context[method].apply(context, routeArgs)
 }
 
 // Secured routes check if a valid req.user exists as per express
 // general practices. Req.logout() clears this. The handler will
 // redirect to a fail redirect in the case of unauthenticated calls.
-function securedRoute (seneca, options, context, method, route) {
-  context[method](route.path, (request, reply, next) => {
-    if (!request.user) {
-      return reply.redirect(route.secure.fail)
-    }
+function securedRoute (seneca, options, context, method, middleware, route) {
+  const routeArgs = [route.path]
+    .concat(middleware)
+    .concat([(request, reply, next) => {
+      if (!request.user) {
+        return reply.redirect(route.secure.fail)
+      }
+      handleRoute(seneca, options, request, reply, route, next)
+    }])
 
-    handleRoute(seneca, options, request, reply, route, next)
-  })
+  context[method].apply(context, routeArgs)
 }
